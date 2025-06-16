82d38b08e23106f7dc96bd57c644d24434fbc332
Utente <giorgianieli@gmail.com>
2025-06-16 09:34:41 +0200

    fix browserrouter per pubblicazione github page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import NotFound from "./pages/NotFound";
 import QuizIntro from "./pages/QuizIntro";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
+    <BrowserRouter basename="/quiz-sostenibile">
       <Route path="/" element={<Home />} />
       <Route path="/quizIntro" element={<QuizIntro />} />
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/result" element={<Result />} />
       <Route path="*" element={<NotFound />} />
-    </Routes>
+    </BrowserRouter>
   );
 }
 
