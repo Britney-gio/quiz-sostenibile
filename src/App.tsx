115bfc04489336676acd19f5d530d2c3152b8a20
Utente <giorgianieli@gmail.com>
2025-06-16 10:03:29 +0200

    problemi di pubblicazione

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import NotFound from "./pages/NotFound";
 import QuizIntro from "./pages/QuizIntro";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/quiz-sostenibile">
-      <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/quizIntro" element={<QuizIntro />} />
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/result" element={<Result />} />
       <Route path="*" element={<NotFound />} />
-    </BrowserRouter>
+    </Routes>
   );
 }
 
